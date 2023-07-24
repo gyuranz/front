3f83479154300ea5f7c9751006be75fe323076de
bigperson <daein7076@gmail.com>
2023-07-24 19:51:10 +0900

    server test ver.1.02

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Login from "./components/Login";
 import Router from "./Router";
 
 // export const MY_URL = "http://localhost:8080";
-export const MY_URL = "15.164.100.230:8080";
+export const MY_URL = "15.164.100.230";
 
 function App() {
     return (
