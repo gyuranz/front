a3ad0767a3e5653d2378e0942a8e1c13be13976c
bigperson <daein7076@gmail.com>
2023-07-27 05:51:40 +0900

    make summary.tsx

--- src/components/room_components/Summary.tsx
+++ src/components/room_components/Summary.tsx
@@ -23,15 +23,6 @@ const Card = styled.div`
     padding: 3px 3px;
 `;
 
-const chunkData = [
-    "2020년, 게이트라는 미지의 차원과 연결된 문이 열려 세계에 변화가 발생함.",
-    "첫 생존자인 헌터들의 등장으로 몬스터와의 전투가 시작되고 세계는 파괴 위협을 겪음.",
-    "퍼스트 헌터들과 후원자들로 인해 세상이 반격을 시작하고 헌터들의 성장과 노력으로 세계를 구원함.",
-    "최악의 게이트 '게헤나'가 발생하고 열두 영웅들의 등장으로 세계는 다시 위험에 빠짐.",
-    "열두 영웅들에 의해 세상은 변화하고 에덴이 만들어짐.",
-    "그러나 잊혀진 마왕과 그의 마법서에 대한 진실은 한 명의 헌터만이 알고 있음.",
-];
-
 //! summay 클릭시 fetch로 summary 데이터를 가져옴.
 //! 가져온 데이터를 배열로 바꾼후 (. 기준 혹은 다른 기준을 정해야 할듯)
 function Summary() {
@@ -50,11 +41,6 @@ function Summary() {
         });
     };
     useEffect(() => {
-        setSummaryArraySet((prev) => {
-            prev = [...chunkData];
-            return chunkData;
-        });
-
         const sendRequest = async () => {
             try {
                 const response = await fetch(
@@ -72,6 +58,10 @@ function Summary() {
                 if (!response.ok) {
                     throw new Error(responseData.message);
                 }
+                setSummaryArraySet((prev) => {
+                    prev = [...responseData.summary];
+                    return responseData.summary;
+                });
                 console.log(responseData);
             } catch (err) {
                 console.log(err);
